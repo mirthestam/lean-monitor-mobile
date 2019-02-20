220f50d3b654f11ac3cd2d38655edd2de512d6ea
Mirthe Stam <mirthe@zephelia.nl>
2019-02-20 18:26:09 +0100

    Simplified generation

--- Assets/Icons/generate.sh
+++ Assets/Icons/generate.sh
@@ -1,55 +1,62 @@
 #!/bin/bash
 
-white='FFFFFF'
-black='000000'
-light='333333'
-dark='AAAAAA'
+#light theme
+light_tabs='337AB7'
 
-export_file() {    
-    export_to_color $1 black $black
-    export_to_color $1 white $white
-    export_to_color $1 light $light
-    export_to_color $1 dark $dark
-}
+# dark theme
+dark_tabs='C2C2C2'
 
 export_to_color() {
                
     # Create SVG for this color
     sed "s/ff0000/$3/g" $1.svg > $1-$2.svg    
     
-    echo $3
-    echo $1
-    echo $2    
-    echo ---
-
+    echo $4
+    
     # Export android images
-    export_from_svg $1-$2 ./android/drawable-mdpi/$1-18-$2.png 18    
-    export_from_svg $1-$2 ./android/drawable-mdpi/$1-24-$2.png 24
-    export_from_svg $1-$2 ./android/drawable-mdpi/$1-36-$2.png 36
-    export_from_svg $1-$2 ./android/drawable-mdpi/$1-48-$2.png 48
-    
-    export_from_svg $1-$2 ./android/drawable-hdpi/$1-18-$2.png 27    
-    export_from_svg $1-$2 ./android/drawable-hdpi/$1-24-$2.png 36
-    export_from_svg $1-$2 ./android/drawable-hdpi/$1-36-$2.png 54
-    export_from_svg $1-$2 ./android/drawable-hdpi/$1-48-$2.png 72
-    
-    export_from_svg $1-$2 ./android/drawable-xhdpi/$1-18-$2.png 36    
-    export_from_svg $1-$2 ./android/drawable-xhdpi/$1-24-$2.png 48
-    export_from_svg $1-$2 ./android/drawable-xhdpi/$1-36-$2.png 72
-    export_from_svg $1-$2 ./android/drawable-xhdpi/$1-48-$2.png 96
+    if [ "$4" == "18" ]; then
+        export_from_svg $1-$2 ./android/drawable-mdpi/$1-18-$2.png 18
+        export_from_svg $1-$2 ./android/drawable-hdpi/$1-18-$2.png 27
+        export_from_svg $1-$2 ./android/drawable-xhdpi/$1-18-$2.png 36
+        export_from_svg $1-$2 ./android/drawable-xxhdpi/$1-18-$2.png 54
+    fi         
 
-    export_from_svg $1-$2 ./android/drawable-xxhdpi/$1-18-$2.png 54    
-    export_from_svg $1-$2 ./android/drawable-xxhdpi/$1-24-$2.png 72
-    export_from_svg $1-$2 ./android/drawable-xxhdpi/$1-36-$2.png 108
-    export_from_svg $1-$2 ./android/drawable-xxhdpi/$1-48-$2.png 144    
+    if [ "$4" == "24" ]; then
+        export_from_svg $1-$2 ./android/drawable-mdpi/$1-24-$2.png 24        
+        export_from_svg $1-$2 ./android/drawable-hdpi/$1-24-$2.png 36
+        export_from_svg $1-$2 ./android/drawable-xhdpi/$1-24-$2.png 48    
+        export_from_svg $1-$2 ./android/drawable-xxhdpi/$1-24-$2.png 72    
+    fi
+    
+    if [ "$4" == "36" ]; then
+        export_from_svg $1-$2 ./android/drawable-mdpi/$1-36-$2.png 36
+        export_from_svg $1-$2 ./android/drawable-hdpi/$1-36-$2.png 54
+        export_from_svg $1-$2 ./android/drawable-xhdpi/$1-36-$2.png 72
+        export_from_svg $1-$2 ./android/drawable-xxhdpi/$1-36-$2.png 108
+    fi
+
+    if [ "$4" == "48" ]; then
+        export_from_svg $1-$2 ./android/drawable-mdpi/$1-48-$2.png 48
+        export_from_svg $1-$2 ./android/drawable-hdpi/$1-48-$2.png 72
+        export_from_svg $1-$2 ./android/drawable-xhdpi/$1-48-$2.png 96
+        export_from_svg $1-$2 ./android/drawable-xxhdpi/$1-48-$2.png 144  
+    fi  
 }
 
 export_from_svg() {
     inkscape --file $1.svg --export-png $2 --export-width $3
 }
 
-export_file baseline-dashboard
-export_file baseline-work
-export_file baseline-description
-export_file baseline-swap_horiz
+# Export all required images here
+export_to_color baseline-dashboard light $light_tabs 24
+export_to_color baseline-dashboard dark $dark_tabs 24
+
+export_to_color baseline-work light $light_tabs 24 
+export_to_color baseline-work dark $dark_tabs 24
+
+export_to_color baseline-description light $light_tabs 24
+export_to_color baseline-description dark $dark_tabs 24
+
+export_to_color baseline-swap_horiz light $light_tabs 24
+export_to_color baseline-swap_horiz dark $dark_tabs 24
 
